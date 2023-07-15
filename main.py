68908c1803806f6d13d9a9afe82f8d27c592fbe4
sainath0246 <65106893+sainath0246@users.noreply.github.com>
2023-07-15 15:02:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template,redirect,request,url_for
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    x=12
+    return render_template("home.html",x=x)
+@app.route('/about')
+def link():
+    return render_template("link.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
